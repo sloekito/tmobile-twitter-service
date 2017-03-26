ae23af447a2944377a84baea8a58f9b68a97c342
Travis Deason <tdeason416@gmail.com>
2017-03-26 10:49:32 -0700

    commit

--- get_analysis.py
+++ get_analysis.py
@@ -15,56 +15,89 @@ application = Flask(__name__)
 api = Api(application)
 CORS(application)
 
+TWITTER_OAUTH_TOKEN = "1733217704-mRoZiblpaY3Dm4PHdusohME83KXfASrnmviJrvS"
+TWITTER_OAUTH_SECRET = "QtFUDlVFkaqbrHgSmbJ952OmwmLNymtADdNcSEI7N6iSl"
+TWITTER_CONSUMER_KEY = "hkF8zr3wn9obDaRf3RSX0O7qk"
+TWITTER_CONSUMER_SECRET = "UqVh1MQkCPdU117MCYm8xeoUJdWlALrZlJ04Sj76ttQOxsrdNz"
+
 
 @application.route("/score/<id>")
 def score(id):
+    auth = OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_OAUTH_TOKEN, TWITTER_OAUTH_SECRET)
+
+    twitterApi = tweepy.API(auth)
     print(id)
     data = {}
     data["id"] = id
     data["score"] = 70
 
+    userInfo = {}
+
     if id == "a":
         data["id"] = id
         data["score"] = 50
 
-    
-    userInfo = getUserInfo(id)
-    data["profile_image_url"] = userInfo["profile_image_url"]
+    result = getUserInfo(twitterApi, id)
+    userInfo = result
+    # print(userInfo)
+    data["screen_name"] = userInfo.screen_name
+    data["location"] = userInfo.location
+    data["description"] = userInfo.description
+    data["profile_image_url"] = userInfo.profile_image_url
+
+    # data["description"] = userInfo["description"]
+    # data["profile_image_url"] = userInfo["profile_image_url"]
+    data["last_tweet"] = userInfo.status.text
     response = application.response_class(
         response=json.dumps(data),
         status=200,
         mimetype='application/json'
     )
 
-    tweets = getTweets(id)
+    # Get last 100 tweets
 
-    print(tweets)
+    tweets = get_tweets(twitterApi, id, 100)
+
+    # Call Travis Api
+
+    # print(tweets)
     return response
 
 
+def getUserInfo(twitterApi, id):
 
-def getUserInfo(id):
-    # userInfo = self.api.user_timeline(screen_name=id, count=100)
-    with open('tweet_user.json') as data_file:
-        data = json.load(data_file)
-        print(data["profile_image_url"])
-    
-        return data
-    return {}
+    # Get the User object for twitter...
+    userInfo = twitterApi.get_user(screen_name=id)
 
-def getTweets(id):
-    with open('tweet_timeline.json') as data_file:
-        data = json.load(data_file)
-        # print(data)
-        texts = []
-        for item in data:
-            texts.append(item["text"])
-        return texts
+    return userInfo
 
-    return {}
-    # statuses = self.api.user_timeline(screen_name=id, count=100)
-    # for status in statuses:
-    #     print(status.text)
+    # with open('tweet_user.json') as data_file:
+    #     data = json.load(data_file)
+    #     print(data["profile_image_url"])
+    #     return data
+    # return {}
+
+def get_tweets(twitterApi, id, count):
+    statuses = twitterApi.user_timeline(screen_name=id, count=count)
+    data = []
+
+    for status in statuses:
+        data.append(status.text)
+
+    jsonData = json.dumps(data) 
+    #print(jsonData)
+    return jsonData
+
+# def getTweets(id):
+#     with open('tweet_timeline.json') as data_file:
+#         data = json.load(data_file)
+#         # print(data)
+#         texts = []
+#         for item in data:
+#             texts.append(item["text"])
+#         return texts
+
 
 
 if __name__ == "__main__":
